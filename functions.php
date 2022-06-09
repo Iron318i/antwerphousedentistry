f179e9377a16b0785bd27a0c49b1fb04409c5372
shilkomariya <shilkomariya@gmail.com>
2022-06-09 13:41:13 +0300

    13-40

--- functions.php
+++ functions.php
@@ -2,16 +2,18 @@
 /**
  * GeneratePress child theme functions and definitions.
  *
- * Add your custom PHP in this file. 
+ * Add your custom PHP in this file.
  * Only edit this file if you have direct access to it on your server (to fix errors if they happen).
  */
 
-function generatepress_child_enqueue_scripts() {
-	if ( is_rtl() ) {
-		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
-	}
+function generatepress_child_enqueue_scripts()
+{
+    if (is_rtl()) {
+        wp_enqueue_style('generatepress-rtl', trailingslashit(get_template_directory_uri()) . 'rtl.css');
+    }
 }
-add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
+
+add_action('wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100);
 
 
 function acf_force_type_array($var)
@@ -35,5 +37,24 @@ function acf_force_type_array($var)
 
 
     // place in array
-    return array( $var );
+    return array($var);
 }
+
+if (!function_exists('antwerphousedentistry_scripts')) {
+
+    /**
+     * Load theme's JavaScript and CSS sources.
+     */
+    function antwerphousedentistry_scripts()
+    {
+        // Get the theme data.
+        $the_theme = wp_get_theme();
+        $theme_version = $the_theme->get('Version');
+        $css_version = $theme_version . '.' . filemtime(get_template_directory() . '/css/theme.min.css');
+
+        wp_enqueue_style('antwerphousedentistry-styles', get_template_directory_uri() . '/css/theme.min.css', array(), $css_version);
+    }
+
+} // End of if function_exists( 'antwerphousedentistry_scripts' ).
+
+add_action('wp_enqueue_scripts', 'antwerphousedentistry_scripts');
