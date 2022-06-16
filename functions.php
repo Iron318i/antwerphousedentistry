c5f1e3aaca0cef5ce788389a3a81a4f5a5a4a9ef
shilkomariya <shilkomariya@gmail.com>
2022-06-16 15:52:56 +0300

    15-50

--- functions.php
+++ functions.php
@@ -71,3 +71,47 @@ function page_header()
 
 add_action('generate_after_header', 'page_header');
 
+
+// Fully Disable Gutenberg editor.
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
+// Don't load Gutenberg-related stylesheets.
+add_action('wp_enqueue_scripts', 'remove_block_css', 100);
+
+function remove_block_css()
+{
+    wp_dequeue_style('wp-block-library'); // WordPress core
+    wp_dequeue_style('wp-block-library-theme'); // WordPress core
+    wp_dequeue_style('wc-block-style'); // WooCommerce
+    wp_dequeue_style('storefront-gutenberg-blocks'); // Storefront theme
+}
+
+/**
+ * Disable the emoji's
+ */
+function disable_emojis()
+{
+    remove_action('wp_head', 'print_emoji_detection_script', 7);
+    remove_action('admin_print_scripts', 'print_emoji_detection_script');
+    remove_action('wp_print_styles', 'print_emoji_styles');
+    remove_action('admin_print_styles', 'print_emoji_styles');
+    remove_filter('the_content_feed', 'wp_staticize_emoji');
+    remove_filter('comment_text_rss', 'wp_staticize_emoji');
+    remove_filter('wp_mail', 'wp_staticize_emoji_for_email');
+
+    // Remove from TinyMCE
+    add_filter('tiny_mce_plugins', 'disable_emojis_tinymce');
+}
+
+add_action('init', 'disable_emojis');
+
+/**
+ * Filter out the tinymce emoji plugin.
+ */
+function disable_emojis_tinymce($plugins)
+{
+    if (is_array($plugins)) {
+        return array_diff($plugins, array('wpemoji'));
+    } else {
+        return array();
+    }
+}
