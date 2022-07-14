7e2172b435c7435e5f73554e78c440b317903cca
shilkomariya <iron318i@gmail.com>
2022-07-14 15:19:24 +0300

    15-10

--- functions.php
+++ functions.php
@@ -50,7 +50,7 @@ if (!function_exists('antwerphousedentistry_scripts')) {
         // Get the theme data.
         $the_theme = wp_get_theme();
         //$css_version = date('H-i', time());
-        $css_version = 1.21;
+        $css_version = 1.22;
         wp_enqueue_style('antwerphousedentistry-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), $css_version);
 
         wp_enqueue_script('masonry-scripts', get_stylesheet_directory_uri() . '/js/masonry.pkgd.min.js', array(), $css_version, true);
@@ -63,7 +63,7 @@ add_action('wp_enqueue_scripts', 'antwerphousedentistry_scripts');
 
 function page_header()
 {
-    $exclude = array("14363", "14368", "14372", "14358", "4297", "4303", "4300");
+    $exclude = array("14363", "14368", "14372", "14358", "4297", "4303", "4300", "679");
     if (!in_array(get_the_ID(), $exclude)) {
         if (is_page_template('page-new.php')) {
             if (has_post_thumbnail()) {
